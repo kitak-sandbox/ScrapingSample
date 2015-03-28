92166a230206e0fd74eb62257f9800b8cd9202c8
Keisuke KITA <kei.kita2501@gmail.com>
2015-03-28 16:32:06 +0900

    バリデーションルールを設定した

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,2 +1,6 @@
 class Pokemon < ActiveRecord::Base
+  validates :number, presence: true
+  validates :name, presence: true
+  validates :height, presence: true
+  validates :specific, presence: true
 end
