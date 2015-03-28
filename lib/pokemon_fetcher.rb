6404d13928ecfbbe0dfaf0594d89990432bc53de
Kohei Makita <nikuzuki29@gmail.com>
2015-03-28 16:21:48 +0900

    名前を取れるようにした

--- lib/pokemon_fetcher.rb
+++ lib/pokemon_fetcher.rb
@@ -1,10 +1,14 @@
 require 'open-uri'
+require 'nokogiri'
 
 module PokemonFetcher
   def fetch(number)
     number = number.to_s.rjust(3, '0')
     html = open("http://www.pokemon.co.jp/zukan/pokemon/#{number}.html").read
-    puts html
+    html_doc = Nokogiri::HTML(html)
+    name = html_doc.at_css("h1").inner_text
+    puts name
   end
   module_function :fetch
 end
+PokemonFetcher.fetch(1)
