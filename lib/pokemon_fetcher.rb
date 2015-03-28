a686099cffd3355b05699a966c723ea61c6239bc
Keisuke KITA <kei.kita2501@gmail.com>
2015-03-28 16:28:19 +0900

    ポケモンの高さと特性をとれるようにした

--- lib/pokemon_fetcher.rb
+++ lib/pokemon_fetcher.rb
@@ -6,9 +6,15 @@ module PokemonFetcher
     number = number.to_s.rjust(3, '0')
     html = open("http://www.pokemon.co.jp/zukan/pokemon/#{number}.html").read
     html_doc = Nokogiri::HTML(html)
-    name = html_doc.at_css("h1").inner_text
-    puts name
+    name = html_doc.at_css("h1").inner_text.strip
+    height = html_doc.at_css('img[alt="高さ"]').parent.next.inner_text.strip
+    specific = html_doc.at_css('img[alt="特性"]').parent.next.inner_text.strip
+
+    {
+      name: name,
+      height: height,
+      specific: specific
+    }
   end
   module_function :fetch
 end
-PokemonFetcher.fetch(1)
